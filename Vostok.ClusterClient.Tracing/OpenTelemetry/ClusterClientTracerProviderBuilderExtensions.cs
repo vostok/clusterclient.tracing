f5f589b3e345cd081e68ae4a2fdc456b917001ba
ponomaryovigor <ponomaryovigor@skbkontur.ru>
2024-10-18 12:57:56 +0200

    Add documentation and make api public.

--- Vostok.ClusterClient.Tracing/OpenTelemetry/ClusterClientTracerProviderBuilderExtensions.cs
+++ Vostok.ClusterClient.Tracing/OpenTelemetry/ClusterClientTracerProviderBuilderExtensions.cs
@@ -1,10 +1,22 @@
-﻿using Vostok.Clusterclient.Tracing.OpenTelemetry;
+﻿using JetBrains.Annotations;
+using Vostok.Clusterclient.Tracing;
+using Vostok.Clusterclient.Tracing.OpenTelemetry;
 
 // ReSharper disable once CheckNamespace
 namespace OpenTelemetry.Trace;
 
-internal static class ClusterClientTracerProviderBuilderExtensions
+/// <summary>
+/// Extension methods to simplify registering of <see cref="Vostok.Clusterclient.Core.IClusterClient" /> instrumentation.
+/// </summary>
+[PublicAPI]
+public static class ClusterClientTracerProviderBuilderExtensions
 {
+    /// <summary>
+    /// Enables the outgoing requests automatic data collection for <see cref="Vostok.Clusterclient.Core.IClusterClient" />.
+    /// </summary>
+    /// <param name="builder"><see cref="TracerProviderBuilder" /> being configured.</param>
+    /// <returns>The instance of <see cref="TracerProviderBuilder"/> to chain the calls.</returns>
+    /// <remarks>Note that you also need to call <see cref="IClusterClientConfigurationExtensions.SetupOpenTelemetryTracing"/> to enable traces emitting for certain <see cref="Vostok.Clusterclient.Core.IClusterClient"/>.</remarks>
     public static TracerProviderBuilder AddClusterClientInstrumentation(this TracerProviderBuilder builder) =>
         builder.AddSource(Instrumentation.ActivitySourceName);
 }
